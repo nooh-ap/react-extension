cf360ded369b079e1c817d6d44dae90c63fd0da4
Nooh AlMahfoodhi <nooh.almahfoodhi@gmail.com>
2023-05-13 20:06:20 -0700

    Added dropdown w/ Styling

--- src/Pages/ValidateData_Flow/ValidateData.js
+++ src/Pages/ValidateData_Flow/ValidateData.js
@@ -3,6 +3,14 @@ import StepsIndicator from "../../Components/Steps-Indicator";
 import Button from "../../Components/Button";
 import "./ValidateData.css";
 
+const otherTitles = [
+  "Software Engineer",
+  "Software Developer",
+  "C++ Developer",
+  "Java Developer",
+  "AWS Architect",
+];
+
 const candidateInfo = {
   title: ["Fullstack Developer"],
   name: ["John Doe"],
@@ -28,12 +36,18 @@ let num = 0;
 const ValidateData = () => {
   const [clickedElement, setClickedElement] = useState("not clicked");
 
-  // useEffect(() => {}, []);
+  useEffect(() => {}, []);
 
-  function handelClick() {
-    setClickedElement("clicked");
+  const openDropddown = () => {
+    clickedElement === "clicked"
+      ? setClickedElement([])
+      : setClickedElement("clicked");
+  };
+
+  const closeDropdown = () => {
+    setClickedElement([]);
     console.log(clickedElement);
-  }
+  };
 
   return (
     <div>
@@ -41,49 +55,63 @@ const ValidateData = () => {
         <h1>Your Info Below</h1>
         <div id="candidate-info">
           {/* TITLE */}
-          <div id="title">
-            <div>
-              <button id="btn_edit" onClick={handelClick}>
+          <div className="title" id="info_container">
+            <div className="info_header">
+              <button id="btn_edit" onClick={openDropddown}>
                 <i className="las la-edit"></i>
               </button>
               <h3>Title</h3>
             </div>
             {clickedElement === "clicked" ? (
               <div id="form_container">
-                <div>
-                  <p>{candidateInfo.title}</p>
+                <div id="form_head">
+                  <div className="form_item_pill">
+                    <p id="form_pill_text">{candidateInfo.title}</p>
+                    <i className="las la-times"></i>
+                  </div>
+                  <input type="text" />
+                  <button id="btn_save" onClick={closeDropdown}>
+                  <i class="las la-check"></i>
+                  </button>
                 </div>
-                <input type="text" />
-                <button id="btn_save" onClick={handelClick}>
-                  <i className="las la-save"></i>
-                </button>
+
+                <div className="dropdownlist_container">
+                  <h5>Select an option or create one</h5>
+
+                  {
+                    otherTitles?.map((title) => (
+                      <li key={num++}>{title}</li>
+                    )) || <p>None</p>
+                    }
+                </div>
               </div>
             ) : (
               <p>{candidateInfo.title}</p>
             )}
           </div>
           {/* EXPERIENCE */}
-          <div id="experience">
-            <div>
-              <button id="btn_edit" onClick={handelClick}>
+          <div id="info_container" className="experience">
+            <div className="info_header">
+              <button id="btn_edit" onClick={openDropddown}>
                 <i className="las la-edit"></i>
               </button>
               <h3>Experience</h3>
             </div>
             <p>{candidateInfo.experience}</p>
           </div>
-          <div id="education">
-            <div>
-              <button id="btn_edit" onClick={handelClick}>
+          {/* EDUCATION */}
+          <div id="info_container" className="education">
+            <div className="info_header">
+              <button id="btn_edit" onClick={openDropddown}>
                 <i className="las la-edit"></i>
               </button>
               <h3>Education</h3>
             </div>
             <p>{candidateInfo.education}</p>
           </div>
-          <div id="skills">
-            <div>
-              <button id="btn_edit" onClick={handelClick}>
+          <div className="skills" id="info_container">
+            <div className="info_header">
+              <button id="btn_edit" onClick={openDropddown}>
                 <i className="las la-edit"></i>
               </button>
               <h3>Key Skills</h3>
@@ -96,19 +124,7 @@ const ValidateData = () => {
             </div>
           </div>
         </div>
-        <div id="overlay">
-          <div id="candidate-resume">
-            <div id="resume">
-              <div>
-                <button id="btn_edit" onClick={handelClick}>
-                  <i className="las la-edit"></i>
-                </button>
-                <h3>Resume</h3>
-              </div>
-              <p>Resume.pdf</p>
-            </div>
-          </div>
-        </div>
+
         <Button />
         <StepsIndicator stepNumber={2} />
       </div>
